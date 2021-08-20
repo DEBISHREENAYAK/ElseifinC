a7d2e566d1fdea050b752bb6a21ac9f0b61bfe6a
DEBISHREENAYAK <57474704+DEBISHREENAYAK@users.noreply.github.com>
2021-08-20 21:05:58 +0530

    Create elseif.c

---
+++ elseif.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+  int a=10;
+  if(a<20)
+    printf("a is less than 20");
+  else if(a>20 & a<100)
+     printf("a is greater than 20");
+  else
+     printf("a ia grether than 100");
+
+    return 0;
+}
